218c2be53395d6a0ec416a1c8b6d53126a53c2d9
Serhii Ilin <serhii.ilin@semanticbits.com>
2021-12-09 10:30:19 -0500

    MAT-3513 Save/Update measure's steward

---
+++ src/main/java/cms/gov/madie/measure/models/MeasureMetaData.java
@@ -0,0 +1,10 @@
+package cms.gov.madie.measure.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class MeasureMetaData {
+  private String measureSteward;
+}
