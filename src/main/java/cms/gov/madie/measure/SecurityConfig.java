793801712423faa22a114c8030eba3ea38963fa4
Cecilia Liu <cecilialiu8@gmail.com>
2022-01-20 13:34:56 -0600

    exclude health from security

--- src/main/java/cms/gov/madie/measure/SecurityConfig.java
+++ src/main/java/cms/gov/madie/measure/SecurityConfig.java
@@ -11,6 +11,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.cors()
+        .disable()
+        .authorizeRequests()
+        .antMatchers("/actuator/**")
+        .permitAll()
         .and()
         .authorizeRequests()
         .anyRequest()
