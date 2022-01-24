085b4853fa11edb1d01cd36ae21cb421ecc243c9
Ashok Dongare <ashok.dongare@semanticbits.com>
2022-01-24 10:11:38 -0500

    MAT-3865 Save test case as embedded collection in measure, move testcases to measure service

--- src/main/java/cms/gov/madie/measure/exceptions/ResourceNotFoundException.java
+++ src/main/java/cms/gov/madie/measure/exceptions/ResourceNotFoundException.java
@@ -1,9 +1,9 @@
 package cms.gov.madie.measure.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-  private static final String ID_MESSAGE = "Could not find %s with id: %s";
+  private static final String MESSAGE = "Could not find %s with id: %s";
 
   public ResourceNotFoundException(String type, String id) {
-    super(String.format(ID_MESSAGE, type, id));
+    super(String.format(MESSAGE, type, id));
   }
 }
