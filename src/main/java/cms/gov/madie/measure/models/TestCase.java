86f1e27b3c7b6a25594c7cc920e8c355ff4452b5
MFHZ <matt.mcphillips@semanticbits.com>
2022-01-31 08:11:11 -0800

    Add title to Java model

--- src/main/java/cms/gov/madie/measure/models/TestCase.java
+++ src/main/java/cms/gov/madie/measure/models/TestCase.java
@@ -14,6 +14,7 @@ import java.util.Date;
 public class TestCase {
   private String id;
   private String name;
+  private String title;
   private String series;
   private String description;
   private Date createdAt;
