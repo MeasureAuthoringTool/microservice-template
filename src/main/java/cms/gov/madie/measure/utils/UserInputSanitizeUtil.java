24eb42ffe053815f26ae3979d2a38369d1b5a9c3
MFHZ <matt.mcphillips@semanticbits.com>
2022-03-02 10:08:54 -0800

    Update utils to prevent trimming additional whitespace to preserve user input

--- src/main/java/cms/gov/madie/measure/utils/UserInputSanitizeUtil.java
+++ src/main/java/cms/gov/madie/measure/utils/UserInputSanitizeUtil.java
@@ -2,6 +2,7 @@ package cms.gov.madie.measure.utils;
 
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Safelist;
+import org.jsoup.nodes.Document;
 
 import io.micrometer.core.instrument.util.StringUtils;
 
@@ -10,7 +11,9 @@ public class UserInputSanitizeUtil {
   public static String sanitizeUserInput(String input) {
     String sanitized = input;
     if (StringUtils.isNotBlank(input)) {
-      sanitized = Jsoup.clean(input, Safelist.basic());
+      sanitized =
+          Jsoup.clean(
+              input, "", Safelist.basic(), new Document.OutputSettings().prettyPrint(false));
       if (StringUtils.isNotBlank(sanitized)) {
         sanitized =
             sanitized.replaceAll("&lt;", "<").replaceAll("&gt;", ">").replaceAll("&amp;", "&");
