0edf286cc6d73c5b2865577c6eed69e4851d0117
CeciliaLiu8 <cecilialiu8@gmail.com>
2022-03-23 08:42:54 -0500

    MAT-4050 log user login and logout backend

--- src/main/java/cms/gov/madie/measure/resources/LoggingController.java
+++ src/main/java/cms/gov/madie/measure/resources/LoggingController.java
@@ -2,10 +2,11 @@ package cms.gov.madie.measure.resources;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.instrument.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,21 +15,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j(topic = "logging")
 public class LoggingController {
 
-  @GetMapping("/getlogger")
-  public ResponseEntity<String> test(@RequestBody String message) {
-    log.info(message);
-    return ResponseEntity.status(HttpStatus.CREATED).body("get test message");
-  }
-
   @PostMapping("/log/login")
   public ResponseEntity<String> loginLog(@RequestBody String message) {
     log.info("loginLog(): " + message);
+    if (StringUtils.isBlank(message)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(HttpStatus.BAD_REQUEST.toString());
+    }
     return ResponseEntity.status(HttpStatus.OK).body("logged login message: " + message);
   }
 
   @PostMapping("/log/logout")
   public ResponseEntity<String> logoutLog(@RequestBody String message) {
     log.info("logoutLog(): " + message);
+    if (StringUtils.isBlank(message)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(HttpStatus.BAD_REQUEST.toString());
+    }
     return ResponseEntity.status(HttpStatus.OK).body("logged logout message: " + message);
   }
 }
---
+++ src/test/java/cms/gov/madie/measure/resources/LoggingControllerMvcTest.java
@@ -0,0 +1,87 @@
+package cms.gov.madie.measure.resources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest({LoggingController.class})
+public class LoggingControllerMvcTest {
+
+  @Autowired private MockMvc mockMvc;
+
+  public static final String USER_INFO =
+      "{\"sub\":\"testSub\",\"email\":\"test.user@test.com\",\"email_verified\":true}";
+
+  @Test
+  public void testLoginLogSuccess() throws Exception {
+    MvcResult result =
+        mockMvc
+            .perform(
+                post("/log/login")
+                    .content(USER_INFO)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andReturn();
+    String response = result.getResponse().getContentAsString();
+    assertTrue(response.contains(USER_INFO));
+  }
+
+  @Test
+  public void testLoginLogBadRequest() throws Exception {
+    MvcResult result =
+        mockMvc
+            .perform(
+                post("/log/login")
+                    .content("")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andReturn();
+    String response = result.getResponse().getContentAsString();
+    assertEquals(response, "");
+  }
+
+  @Test
+  public void testLogoutLogSuccess() throws Exception {
+    MvcResult result =
+        mockMvc
+            .perform(
+                post("/log/logout")
+                    .content(USER_INFO)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andReturn();
+    String response = result.getResponse().getContentAsString();
+    assertTrue(response.contains(USER_INFO));
+  }
+
+  @Test
+  public void testLogoutLogBadRequest() throws Exception {
+    MvcResult result =
+        mockMvc
+            .perform(
+                post("/log/logout")
+                    .content("")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andReturn();
+    String response = result.getResponse().getContentAsString();
+    assertEquals(response, "");
+  }
+}
