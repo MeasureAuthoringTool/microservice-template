6f4da3e02a376c98eef29ef1bd93d4bdddafec19
adongare <ashok.dongare@semanticbits.com>
2022-03-23 19:00:27 -0400

    MAT-3965 transfer mat measure to madie(lambda)

--- src/main/java/cms/gov/madie/measure/SecurityConfig.java
+++ src/main/java/cms/gov/madie/measure/SecurityConfig.java
@@ -1,19 +1,27 @@
 package cms.gov.madie.measure;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private static final String[] CSRF_WHITELIST = {"/measure-transfer/**"};
+  private static final String[] AUTH_WHITELIST = {"/measure-transfer/**", "/actuator/**"};
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.cors()
         .and()
+        .csrf()
+        .ignoringAntMatchers(CSRF_WHITELIST)
+        .and()
         .authorizeRequests()
-        .antMatchers("/actuator/**")
+        .antMatchers(AUTH_WHITELIST)
         .permitAll()
         .and()
         .authorizeRequests()
