0caae2f8e19352a1dce57b43e4727bd4fede0ca3
Nate Moraca <nate.moraca@semanticbits.com>
2022-05-09 14:54:52 -0400

    refactors and tests; ref #MAT-4260

--- src/main/java/cms/gov/madie/measure/exceptions/InvalidResourceBundleStateException.java
+++ src/main/java/cms/gov/madie/measure/exceptions/InvalidResourceBundleStateException.java
@@ -1,7 +1,7 @@
 package cms.gov.madie.measure.exceptions;
 
 public class InvalidResourceBundleStateException extends RuntimeException {
-  private static final String MESSAGE = "Cannot bundle resource %s with ID %s while errors exist.";
+  private static final String MESSAGE = "Cannot bundle resource %s with ID %s while CQL errors exist.";
 
   public InvalidResourceBundleStateException(String type, String id) {
     super(String.format(MESSAGE, type, id));
