a16c9cf17d077d2258b5a8e5aead19c8876fca1d
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2022-05-31 13:21:38 -0400

    Updated the response messages to be generic

--- src/main/java/cms/gov/madie/measure/exceptions/InvalidResourceBundleStateException.java
+++ src/main/java/cms/gov/madie/measure/exceptions/InvalidResourceBundleStateException.java
@@ -1,7 +1,7 @@
 package cms.gov.madie.measure.exceptions;
 
 public class InvalidResourceBundleStateException extends RuntimeException {
-  private static final String MESSAGE = "Cannot bundle resource %s with ID %s while %s";
+  private static final String MESSAGE = "Response could not be completed for %s with ID %s, %s";
 
   public InvalidResourceBundleStateException(String type, String id, String cause) {
     super(String.format(MESSAGE, type, id, cause));
--- src/main/java/cms/gov/madie/measure/resources/MeasureController.java
+++ src/main/java/cms/gov/madie/measure/resources/MeasureController.java
@@ -170,13 +170,13 @@ public class MeasureController {
       throw new UnauthorizedException("Measure", measureId, principal.getName());
     }
     if (measure.isCqlErrors()) {
-      throw new InvalidResourceBundleStateException("Measure", measureId, "CQL errors exist.");
+      throw new InvalidResourceBundleStateException("Measure", measureId, "since CQL errors exist.");
     }
     if (CollectionUtils.isEmpty(measure.getGroups())) {
-      throw new InvalidResourceBundleStateException("Measure", measureId, "there are no groups.");
+      throw new InvalidResourceBundleStateException("Measure", measureId, "since there are no associated measure groups.");
     }
     if (measure.getElmJson() == null) {
-      throw new InvalidResourceBundleStateException("Measure", measureId, "there is no ELM Json.");
+      throw new InvalidResourceBundleStateException("Measure", measureId, "since there are issues with the CQL.");
     }
     System.out.println(measure.getElmJson());
     return ResponseEntity.ok(measureService.bundleMeasure(measure, accessToken));
