161b4d96e61c787c682268044118bcbba6bfcefc
CeciliaLiu8 <cecilialiu8@gmail.com>
2022-07-21 09:37:54 -0500

    remove unused imports

--- src/test/java/cms/gov/madie/measure/resources/MeasureTransferControllerTest.java
+++ src/test/java/cms/gov/madie/measure/resources/MeasureTransferControllerTest.java
@@ -17,11 +17,9 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
