8e38d837bd7e75979ec60392734be7dea99848e8
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2022-08-04 16:05:48 -0400

    removed unused imports

--- src/main/java/cms/gov/madie/measure/resources/UtilsController.java
+++ src/main/java/cms/gov/madie/measure/resources/UtilsController.java
@@ -4,10 +4,10 @@ import cms.gov.madie.measure.repositories.PopulationBasisRepository;
 import gov.cms.madie.models.common.PopulationBasis;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,14 +18,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UtilsController {
 
-  @Autowired private PopulationBasisRepository populationBasisRepository;
+  private final PopulationBasisRepository populationBasisRepository;
 
   @Cacheable("populationBasisValues")
   @GetMapping("/populationBasisValues")
   public ResponseEntity<List<String>> getAllPopulationBasisValues() {
-    // add caching also log some debug
     List<PopulationBasis> populationBasisList = populationBasisRepository.findAll();
-    if (populationBasisList.isEmpty()) {
+    if (CollectionUtils.isEmpty(populationBasisList)) {
       log.debug("No Population Basis values are available");
       throw new RuntimeException("No Population Basis values are available");
     }
