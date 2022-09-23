67106ccd11fce8fc7b5ad28e29c87a0e41d0dc92
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2022-09-23 11:15:18 -0400

    API to add new organizations

--- src/main/java/cms/gov/madie/measure/resources/OrganizationController.java
+++ src/main/java/cms/gov/madie/measure/resources/OrganizationController.java
@@ -4,22 +4,26 @@ import cms.gov.madie.measure.repositories.OrganizationRepository;
 import gov.cms.madie.models.common.Organization;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class OrganizationController {
+
+  private static final String HARP_ID_HEADER = "harp-id";
   private final OrganizationRepository organizationRepository;
 
   @Cacheable("organizations")
@@ -33,13 +37,26 @@ public class OrganizationController {
     return ResponseEntity.status(HttpStatus.OK).body(organizationList);
   }
 
-  //  @PostMapping("/organizations")
-  //  public ResponseEntity<List<Organization>> addOrganizations(
-  //      @RequestBody List<Organization> organizations, Principal principal) {
-  //    final String userName = principal.getName();
-  //    log.info("User [{}] is attempting to add new organizations", organizations);
-  //    List<Organization> organizationList = organizationRepository.saveAll(organizations);
-  //    log.info("User [{}] successfully added new organizations", userName);
-  //    return ResponseEntity.status(HttpStatus.CREATED).body(organizationList);
-  //  }
+  @PostMapping("/organizations")
+  @PreAuthorize("#request.getHeader('api-key') == #apiKey")
+  public ResponseEntity<List<Organization>> addOrganizations(
+      HttpServletRequest request,
+      @RequestBody List<Organization> organizations,
+      @Value("${organizations-api-key}") String apiKey) {
+    final String userName = request.getHeader(HARP_ID_HEADER);
+    log.info("User {} is attempting to add new organizations {}", userName, organizations);
+    try {
+      List<Organization> savedOrganizations = organizationRepository.saveAll(organizations);
+      log.info("User {} successfully added new organizations", userName);
+      return ResponseEntity.status(HttpStatus.CREATED).body(savedOrganizations);
+    } catch (Exception exception) {
+      if (exception instanceof org.springframework.dao.DuplicateKeyException) {
+        log.error("One of the organizations is already available with same OID");
+        throw new DuplicateKeyException(exception.getLocalizedMessage(), "Duplicate oid found");
+      } else {
+        log.error("Error saving organizations");
+        throw new RuntimeException("Error saving organizations");
+      }
+    }
+  }
 }
