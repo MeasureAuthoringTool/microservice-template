e152020037f6a3c74dc225f75ac0b70902d6fe0b
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2022-11-11 09:50:58 -0500

    added test casee

--- src/test/java/cms/gov/madie/measure/service/TestCaseServiceTest.java
+++ src/test/java/cms/gov/madie/measure/service/TestCaseServiceTest.java
@@ -501,6 +501,14 @@ public class TestCaseServiceTest {
   }
 
   @Test
+  void testHasPermissionToDeleteReturnsExceptionThrowsAccessException() {
+    final Measure measure = Measure.builder().id("measure-id").createdBy("OtherUser").build();
+    assertThrows(
+            UnauthorizedException.class,
+            () -> testCaseService.hasPermissionToDelete("measure-id", "user2", measure));
+  }
+
+  @Test
   void testDeleteTestCasReturnsExceptionForGroupNotFoundInMeasure() {
     List<TestCase> testCases =
         List.of(
