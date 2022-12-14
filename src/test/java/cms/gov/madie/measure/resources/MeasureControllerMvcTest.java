68f604c119d602b42f38b8e394095ead8c72f1cb
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2022-12-14 14:59:06 -0500

    fixed test cases

--- src/test/java/cms/gov/madie/measure/resources/MeasureControllerMvcTest.java
+++ src/test/java/cms/gov/madie/measure/resources/MeasureControllerMvcTest.java
@@ -1373,7 +1373,7 @@ public class MeasureControllerMvcTest {
                 .accept(MediaType.APPLICATION_JSON))
         .andDo(print())
         .andExpect(status().isBadRequest())
-        .andExpect(jsonPath("$.message").value("Selected function 'fun' can not have parameters"));
+        .andExpect(jsonPath("$.message").value("Selected observation function 'fun' can not have parameters"));
 
     verify(groupService, times(1))
         .createOrUpdateGroup(
