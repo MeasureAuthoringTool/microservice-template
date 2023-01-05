8e121187beb2ececc5e279da501f443388d6307a
adongare <ashok.dongare@semanticbits.com>
2023-01-05 16:15:20 -0500

    MAT-5179 Add Included library CQL in Measure Bundle

--- src/main/java/cms/gov/madie/measure/services/ExportService.java
+++ src/main/java/cms/gov/madie/measure/services/ExportService.java
@@ -1,18 +1,24 @@
 package cms.gov.madie.measure.services;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
+import ca.uhn.fhir.context.FhirContext;
 import cms.gov.madie.measure.utils.ExportFileNamesUtil;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Service;
 import gov.cms.madie.models.measure.Measure;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.hl7.fhir.r4.model.Attachment;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Library;
+import org.hl7.fhir.r4.model.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Slf4j
 @Service
@@ -20,52 +26,86 @@ import lombok.extern.slf4j.Slf4j;
 public class ExportService {
 
   private final BundleService bundleService;
+  private FhirContext fhirContext;
+
+  private static final String TEXT_CQL = "text/cql";
+  private static final String CQL_DIRECTORY = "/cql/";
 
   public void generateExports(Measure measure, String accessToken, OutputStream outputStream) {
-    String fileName = ExportFileNamesUtil.getExportFileName(measure);
-    log.info("Generating exports for " + fileName);
+    String exportFileName = ExportFileNamesUtil.getExportFileName(measure);
+    log.info("Generating exports for " + exportFileName);
 
-    List<String> exportFiles = new ArrayList<>();
-    exportFiles.add(fileName + ".json");
-    exportFiles.add("/cql/" + measure.getCqlLibraryName() + ".cql");
-
+    String measureBundle = bundleService.bundleMeasure(measure, accessToken);
+    Bundle bundle = createFhirResourceFromJson(measureBundle, Bundle.class);
+    getCQLForLibraries(bundle);
     try (ZipOutputStream zos = new ZipOutputStream(outputStream)) {
-      for (String filePath : exportFiles) {
-        String data = getData(filePath, measure, accessToken);
-        if (StringUtils.isNotBlank(data)) {
-          addBytesToZip(filePath, data.getBytes(), zos);
-        }
-      }
+      addMeasureBundleToExport(zos, exportFileName, measureBundle);
+      addLibraryCqlFilesToExport(zos, bundle);
     } catch (Exception ex) {
       log.error(ex.getMessage());
       throw new RuntimeException(
-          "Unexpected error while generating exports for measureID: " + measure.getId());
+        "Unexpected error while generating exports for measureID: " + measure.getId());
     }
   }
+
+  public <T extends Resource> T createFhirResourceFromJson(String json, Class<T> clazz) {
+    if (StringUtils.isBlank(json)) {
+      return null;
+    }
+    return fhirContext.newJsonParser().parseResource(clazz, json);
+  }
+
+  private void addMeasureBundleToExport(ZipOutputStream zos, String fileName, String measureBundle) throws IOException {
+    String measureBundleFile = fileName + ".json";
+    addBytesToZip(measureBundleFile, measureBundle.getBytes(), zos);
+  }
+
+  private void addLibraryCqlFilesToExport(ZipOutputStream zos, Bundle measureBundle) throws IOException {
+    Map<String, String> cqlMap = getCQLForLibraries(measureBundle);
+    for (Map.Entry<String, String> entry : cqlMap.entrySet()) {
+      String filePath = CQL_DIRECTORY + entry.getKey() + ".cql";
+      String data = entry.getValue();
+      addBytesToZip(filePath, data.getBytes(), zos);
+    }
+  }
+
+
+  private Map<String, String> getCQLForLibraries(Bundle measureBundle) {
+    Map<String, String> libraryCqlMap = new HashMap<String, String>();
+    measureBundle.getEntry()
+      .stream()
+      .filter(entry -> StringUtils.equals("Library", entry.getResource().getResourceType().name()))
+      .forEach(entry -> {
+        Library library = (Library) entry.getResource();
+        Attachment attachment = getCqlAttachment(library);
+        String cql = new String(attachment.getData());
+        libraryCqlMap.put(library.getName(), cql);
+      });
+    return libraryCqlMap;
+  }
+
+  private Attachment getCqlAttachment(Library library) {
+    return library.getContent().stream()
+      .filter(content -> StringUtils.equals(TEXT_CQL, content.getContentType()))
+      .findAny()
+      .orElse(null);
+  }
+
 
   /**
    * Adds the bytes to zip.
    *
-   * @param path file name along with path and extension
-   * @param input the input byte array
+   * @param path            file name along with path and extension
+   * @param input           the input byte array
    * @param zipOutputStream the zip
    * @throws Exception the exception
    */
   private void addBytesToZip(String path, byte[] input, ZipOutputStream zipOutputStream)
-      throws IOException {
+    throws IOException {
     ZipEntry entry = new ZipEntry(path);
     entry.setSize(input.length);
     zipOutputStream.putNextEntry(entry);
     zipOutputStream.write(input);
     zipOutputStream.closeEntry();
-  }
-
-  private String getData(String filePath, Measure measure, String accessToken) {
-    if (filePath.contains(".cql")) {
-      return measure.getCql();
-    } else if (filePath.contains(".json")) {
-      return bundleService.bundleMeasure(measure, accessToken);
-    }
-    return null;
   }
 }
