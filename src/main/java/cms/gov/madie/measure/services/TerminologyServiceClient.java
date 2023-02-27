8d355887943366beadfbf82104ccfade0846f07a
adongare <ashok.dongare@semanticbits.com>
2023-02-27 16:55:42 -0500

    MAT-5323 unused imports

--- src/main/java/cms/gov/madie/measure/services/TerminologyServiceClient.java
+++ src/main/java/cms/gov/madie/measure/services/TerminologyServiceClient.java
@@ -9,13 +9,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
