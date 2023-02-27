50073d1db5e7f49d7ae62923256f654ca4b09841
adongare <ashok.dongare@semanticbits.com>
2023-02-27 17:33:43 -0500

    MAT-5323 removed unused var

--- src/test/java/cms/gov/madie/measure/services/TerminologyServiceClientTest.java
+++ src/test/java/cms/gov/madie/measure/services/TerminologyServiceClientTest.java
@@ -52,7 +52,6 @@ public class TerminologyServiceClientTest {
 
   @Test
   public void testFetchValueSetsWhenNotValueSetNotFound() {
-    var testVs = "two test value sets";
     var oids = List.of("1.2.3.4", "4.5.6.7");
     var criteria =
         ValueSetsSearchCriteria.builder()
