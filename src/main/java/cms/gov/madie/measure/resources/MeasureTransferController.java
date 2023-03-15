092f4ea0aabe6b7df910cb54491cf6f4d71e4d5a
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2023-03-15 13:24:39 -0400

    Updated transfer measure API to store steward and developers along with newly added org URL

--- src/main/java/cms/gov/madie/measure/resources/MeasureTransferController.java
+++ src/main/java/cms/gov/madie/measure/resources/MeasureTransferController.java
@@ -1,6 +1,8 @@
 package cms.gov.madie.measure.resources;
 
+import cms.gov.madie.measure.repositories.OrganizationRepository;
 import gov.cms.madie.models.common.ActionType;
+import gov.cms.madie.models.common.Organization;
 import gov.cms.madie.models.measure.ElmJson;
 import gov.cms.madie.models.measure.Measure;
 import gov.cms.madie.models.measure.MeasureMetaData;
@@ -17,11 +19,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.CollectionUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -33,6 +43,8 @@ public class MeasureTransferController {
   private final MeasureService measureService;
   private final ActionLogService actionLogService;
   private final ElmTranslatorClient elmTranslatorClient;
+
+  private final OrganizationRepository organizationRepository;
 
   @PostMapping("/mat-measures")
   @PreAuthorize("#request.getHeader('api-key') == #apiKey")
@@ -55,13 +67,15 @@ public class MeasureTransferController {
     measure.setLastModifiedAt(now);
     if (measure.getMeasureMetaData() != null) {
       measure.getMeasureMetaData().setDraft(true);
+      updateStewardAndDevelopers(measure);
     } else {
       MeasureMetaData metaData = new MeasureMetaData();
       metaData.setDraft(true);
       measure.setMeasureMetaData(metaData);
+      updateStewardAndDevelopers(measure);
     }
     // set ids for groups
-    measure.getGroups().stream().forEach(group -> group.setId(ObjectId.get().toString()));
+    measure.getGroups().forEach(group -> group.setId(ObjectId.get().toString()));
     Measure savedMeasure = repository.save(measure);
     log.info("Measure [{}] transfer complete", measure.getMeasureName());
 
@@ -71,7 +85,78 @@ public class MeasureTransferController {
     return ResponseEntity.status(HttpStatus.CREATED).body(savedMeasure);
   }
 
-  protected void setMeasureElmJsonAndErrors(Measure measure, String apiKey, String harpId) {
+  private void updateStewardAndDevelopers(Measure measure) {
+    List<Organization> organizationList = organizationRepository.findAll();
+    if (CollectionUtils.isEmpty(organizationList)) {
+      log.debug(
+          "No organizations are available while transferring MAT measure : [{}]",
+          measure.getMeasureName());
+      throw new RuntimeException("No organizations are available");
+    }
+    updateOrganizationName(measure);
+    Organization steward = measure.getMeasureMetaData().getSteward();
+    if (steward != null) {
+      Optional<Organization> stewardOrg =
+          organizationList.stream()
+              .filter(org -> org.getName().equalsIgnoreCase(steward.getName()))
+              .findFirst();
+      measure.getMeasureMetaData().setSteward(stewardOrg.orElse(null));
+    }
+
+    List<Organization> developersList = measure.getMeasureMetaData().getDevelopers();
+    if (developersList != null && !developersList.isEmpty()) {
+      List<Organization> developersOrgs =
+          organizationList.stream()
+              .filter(
+                  org ->
+                      developersList.stream()
+                          .anyMatch(
+                              developer -> developer.getName().equalsIgnoreCase(org.getName())))
+              .toList();
+      measure.getMeasureMetaData().setDevelopers(developersOrgs);
+    }
+  }
+
+  /**
+   * @param measure mat measure There has been few updates to org names in MADiE when compared to
+   *     orgs in MAT. updateOrganizationName will update the steward & developers names, so that
+   *     they can match with updated org names in MADiE
+   */
+  private void updateOrganizationName(Measure measure) {
+    Map<String, String> modifiedOrganizations = new HashMap<>();
+    modifiedOrganizations.put(
+        "American College of Cardiology - ACCF/AHA Task Force on Performance Measures",
+        "American College of Cardiology - ACC/AHA Task Force on Performance Measures");
+    modifiedOrganizations.put(
+        "Arizona State University - Dept of Biomedical Infomatics",
+        "Arizona State University - Dept of Biomedical Informatics");
+    modifiedOrganizations.put("CancerLin Q", "CancerLinQ");
+    modifiedOrganizations.put("Innovaccer Anylytics", "Innovaccer");
+    modifiedOrganizations.put("Intermoutain Healthcare", "Intermountain Healthcare");
+
+    Organization steward = measure.getMeasureMetaData().getSteward();
+    if (steward != null && modifiedOrganizations.containsKey(steward.getName())) {
+      measure
+          .getMeasureMetaData()
+          .getSteward()
+          .setName(modifiedOrganizations.get(steward.getName()));
+    }
+
+    List<Organization> developersList = measure.getMeasureMetaData().getDevelopers();
+    if (developersList != null && !developersList.isEmpty()) {
+      measure
+          .getMeasureMetaData()
+          .getDevelopers()
+          .forEach(
+              d -> {
+                if (modifiedOrganizations.containsKey(d.getName())) {
+                  d.setName(modifiedOrganizations.get(d.getName()));
+                }
+              });
+    }
+  }
+
+  private void setMeasureElmJsonAndErrors(Measure measure, String apiKey, String harpId) {
     try {
       final ElmJson elmJson =
           elmTranslatorClient.getElmJsonForMatMeasure(measure.getCql(), apiKey, harpId);
