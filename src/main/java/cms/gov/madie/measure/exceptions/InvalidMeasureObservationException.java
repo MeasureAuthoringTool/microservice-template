aa0f5b2ed61db53cbb63db7688dfd8026ac9ed59
CeciliaLiu8 <cecilialiu8@gmail.com>
2023-04-12 06:46:07 -0500

    MAT-5569 valid measure observation when creating or updating a group

--- src/main/java/cms/gov/madie/measure/exceptions/InvalidMeasureObservationException.java
+++ src/main/java/cms/gov/madie/measure/exceptions/InvalidMeasureObservationException.java
@@ -2,9 +2,9 @@ package cms.gov.madie.measure.exceptions;
 
 public class InvalidMeasureObservationException extends RuntimeException {
 
-	private static final long serialVersionUID = 1361477038206715878L;
+  private static final long serialVersionUID = 1361477038206715878L;
 
-	public InvalidMeasureObservationException(String message) {
+  public InvalidMeasureObservationException(String message) {
     super(message);
   }
 }
