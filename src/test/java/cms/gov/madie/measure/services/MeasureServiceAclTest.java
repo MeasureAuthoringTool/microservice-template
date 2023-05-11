fd07dafb37c34f73d715b2ded97458ce7c8b0a63
adongare <ashok.dongare@semanticbits.com>
2023-05-11 08:54:44 -0400

    MAT-5587 Optimize imports

--- src/test/java/cms/gov/madie/measure/services/MeasureServiceAclTest.java
+++ src/test/java/cms/gov/madie/measure/services/MeasureServiceAclTest.java
@@ -1,24 +1,23 @@
 package cms.gov.madie.measure.services;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
-import java.util.ArrayList;
-import java.util.Optional;
-
+import cms.gov.madie.measure.repositories.MeasureRepository;
+import gov.cms.madie.models.access.AclSpecification;
+import gov.cms.madie.models.measure.Measure;
 import gov.cms.madie.models.measure.MeasureSet;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import cms.gov.madie.measure.repositories.MeasureRepository;
-import gov.cms.madie.models.access.AclSpecification;
-import gov.cms.madie.models.access.RoleEnum;
-import gov.cms.madie.models.measure.Measure;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class MeasureServiceAclTest {
