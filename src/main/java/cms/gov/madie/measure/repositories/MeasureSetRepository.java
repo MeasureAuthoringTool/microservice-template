2a35a6112c93477c7bac4ab7b7c5fc27c118e214
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2023-05-11 13:53:34 -0400

    removed unwanted imports

--- src/main/java/cms/gov/madie/measure/repositories/MeasureSetRepository.java
+++ src/main/java/cms/gov/madie/measure/repositories/MeasureSetRepository.java
@@ -1,7 +1,6 @@
 package cms.gov.madie.measure.repositories;
 
 import gov.cms.madie.models.measure.MeasureSet;
-import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;;
 
 public interface MeasureSetRepository extends MongoRepository<MeasureSet, String> {
--- src/test/java/cms/gov/madie/measure/services/MeasureSetServiceTest.java
+++ src/test/java/cms/gov/madie/measure/services/MeasureSetServiceTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import java.util.Optional;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
