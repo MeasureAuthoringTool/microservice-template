f2ce3b2936fbda4843b8dae37e4f9f69e6f37344
adongare <ashok.dongare@semanticbits.com>
2023-05-16 08:49:30 -0400

    MAT-5587 update find my measures query to use measure set(owner and acls)

---
+++ src/main/java/cms/gov/madie/measure/repositories/MeasureAclRepository.java
@@ -0,0 +1,17 @@
+package cms.gov.madie.measure.repositories;
+
+import gov.cms.madie.models.measure.Measure;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface MeasureAclRepository {
+  /**
+   * Measure is considered to be my measure if provided user is the owner of this measure or is
+   * shared with provided user and measure is active(measure.active = true)
+   *
+   * @param userId- current user
+   * @param pageable- instance of Pageable
+   * @return Pageable List of measures
+   */
+  Page<Measure> findMyActiveMeasures(String userId, Pageable pageable);
+}
