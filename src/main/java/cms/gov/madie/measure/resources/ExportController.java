4fd34db6821ea54c5e19c2769724e0695ca62596
Greg Akins <gregory.akins@icf.com>
2023-10-05 07:36:21 -0400

    MAT 6204: Modifying test case bundle to transaction on export

--- src/main/java/cms/gov/madie/measure/resources/ExportController.java
+++ src/main/java/cms/gov/madie/measure/resources/ExportController.java
@@ -7,15 +7,33 @@ import cms.gov.madie.measure.services.FhirServicesClient;
 import cms.gov.madie.measure.utils.ControllerUtil;
 import cms.gov.madie.measure.utils.ExportFileNamesUtil;
 import gov.cms.madie.models.measure.Measure;
+import gov.cms.madie.models.measure.TestCase;
+
 import java.security.Principal;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+
+import org.apache.commons.lang3.SerializationUtils;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Configuration;
 
 @Slf4j
 @RestController
@@ -58,6 +76,7 @@ public class ExportController {
       Principal principal,
       @RequestHeader("Authorization") String accessToken,
       @PathVariable String measureId,
+      @PathVariable Optional<String> bundleType,
       @RequestBody List<String> testCaseId) {
 
     final String username = principal.getName();
@@ -70,7 +89,84 @@ public class ExportController {
     }
 
     Measure measure = measureOptional.get();
+    // change Measure Bundle Type to "type" for export
+    switch (BundleType.valueOf(bundleType.orElse(("COLLECTION")))) {
+      case COLLECTION:
+        log.debug("You're exporting a Collection");
+        // update bundle type for each entry MAT 6405
+        // break ;
+      case TRANSACTION:
+        log.debug("You're exporting a Transaction");
+        // update bundle type and add entry.request for each entry
+        if (measure.getTestCases() != null) {
+          measure.getTestCases().stream().forEach(testCase -> updateEntry(testCase));
+        }
+        break;
+      default:
+    }
 
     return fhirServicesClient.getTestCaseExports(measure, accessToken, testCaseId);
+  }
+
+  private void updateEntry(TestCase testCase) {
+    String json =
+        JsonPath.parse(testCase.getJson())
+            .map(
+                "$.type",
+                ((curVal, config) -> {
+                  return "transaction";
+                }))
+            .map(
+                "$.entry",
+                (curVal, config) -> {
+                  log.info("Entries ", curVal);
+                  JSONArray entryArr = (JSONArray) curVal;
+
+                  entryArr.forEach(
+                      entry -> {
+                        JSONObject request = new JSONObject();
+
+                        request.put("method", "PUT");
+                        String resourceType =
+                            (String)
+                                ((LinkedHashMap<String, LinkedHashMap>) entry)
+                                    .get("resource")
+                                    .get("resourceType");
+                        String id =
+                            (String)
+                                ((LinkedHashMap<String, LinkedHashMap>) entry)
+                                    .get("resource")
+                                    .get("id");
+                        // ((LinkedHashMap<String, Object>) entry)
+                        request.put("url", String.format("%s/%s", resourceType, id));
+
+                        ((LinkedHashMap<String, Object>) entry).put("request", request);
+                        log.info("Entry ${}", entry);
+                      });
+
+                  return entryArr;
+                })
+            .jsonString();
+
+    testCase.setJson(json);
+  }
+}
+
+enum BundleType {
+  TRANSACTION("transaction"),
+  COLLECTION("collection");
+
+  private final String bundleType;
+
+  private BundleType(String s) {
+    bundleType = s;
+  }
+
+  public boolean equalsType(String otherType) {
+    return bundleType.equals(otherType);
+  }
+
+  public String toString() {
+    return this.bundleType;
   }
 }
--- src/test/java/cms/gov/madie/measure/resources/ExportControllerTest.java
+++ src/test/java/cms/gov/madie/measure/resources/ExportControllerTest.java
@@ -11,7 +11,14 @@ import cms.gov.madie.measure.services.BundleService;
 import cms.gov.madie.measure.services.FhirServicesClient;
 import gov.cms.madie.models.common.Version;
 import gov.cms.madie.models.measure.Measure;
+import gov.cms.madie.models.measure.TestCase;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -61,13 +68,21 @@ class ExportControllerTest {
   }
 
   @Test
-  void getTestCaseExportAll() {
+  void getTestCaseExportAll() throws IOException {
     Principal principal = mock(Principal.class);
     when(principal.getName()).thenReturn("test.user");
+    List<TestCase> testCases = new ArrayList<>();
+    File jsonFile =
+        new File(this.getClass().getResource("/test_case_exported_json.json").getFile());
+
+    String jsonData = new String(Files.readAllBytes(jsonFile.toPath()));
+
+    testCases.add(TestCase.builder().json(jsonData).build());
     final Measure measure =
         Measure.builder()
             .ecqmTitle("test_ecqm_title")
             .version(new Version(0, 0, 0))
+            .testCases(testCases)
             .model("QiCore 4.1.1")
             .createdBy("test.user")
             .build();
@@ -79,6 +94,7 @@ class ExportControllerTest {
             principal,
             "access-token",
             "example-measure-id",
+            Optional.of("COLLECTION"),
             asList("example-test-case-id-1", "example-test-case-id-2"));
     assertEquals(HttpStatus.OK, output.getStatusCode());
   }
@@ -102,6 +118,31 @@ class ExportControllerTest {
             principal,
             "access-token",
             "example-measure-id",
+            Optional.of("COLLECTION"),
+            asList("example-test-case-id-1", "example-test-case-id-2"));
+    assertEquals(HttpStatus.PARTIAL_CONTENT, output.getStatusCode());
+  }
+
+  @Test
+  void getTestCaseExportAllPartialContentWithDefaultBundleType() {
+    Principal principal = mock(Principal.class);
+    when(principal.getName()).thenReturn("test.user");
+    final Measure measure =
+        Measure.builder()
+            .ecqmTitle("test_ecqm_title")
+            .version(new Version(0, 0, 0))
+            .model("QiCore 4.1.1")
+            .createdBy("test.user")
+            .build();
+    when(measureRepository.findById(anyString())).thenReturn(Optional.of(measure));
+    when(fhirServicesClient.getTestCaseExports(any(Measure.class), anyString(), anyList()))
+        .thenReturn(new ResponseEntity<byte[]>(HttpStatus.PARTIAL_CONTENT));
+    ResponseEntity<byte[]> output =
+        exportController.getTestCaseExport(
+            principal,
+            "access-token",
+            "example-measure-id",
+            Optional.empty(),
             asList("example-test-case-id-1", "example-test-case-id-2"));
     assertEquals(HttpStatus.PARTIAL_CONTENT, output.getStatusCode());
   }
@@ -117,6 +158,7 @@ class ExportControllerTest {
                 principal,
                 "access-token",
                 "example-measure-id",
+                Optional.of("COLLECTION"),
                 asList("example-test-case-id-1", "example-test-case-id-2")));
   }
 }
