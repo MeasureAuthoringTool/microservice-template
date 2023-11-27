050c31c2d27fdd2a899739147f192272949c23d2
CeciliaLiu8 <cecilialiu8@gmail.com>
2023-11-27 13:42:29 -0600

    MAT-6428 migrate SDE data to single field

---
+++ src/main/java/cms/gov/madie/measure/config/MigrateSupplementalDataElementsChangeUnit.java
@@ -0,0 +1,74 @@
+package cms.gov.madie.measure.config;
+
+import java.util.List;
+
+import com.nimbusds.oauth2.sdk.util.CollectionUtils;
+
+import cms.gov.madie.measure.repositories.MeasureRepository;
+import gov.cms.madie.models.measure.Measure;
+import gov.cms.madie.models.measure.MeasureMetaData;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ChangeUnit(id = "migrate_supplemental_data_elements", order = "1", author = "madie_dev")
+public class MigrateSupplementalDataElementsChangeUnit {
+
+  @Setter private List<Measure> tempMeasures;
+
+  @Execution
+  public void migrateSupplementalDataElements(MeasureRepository measureRepository)
+      throws Exception {
+    log.debug("Entering migrateSupplementalDataElements()");
+
+    List<Measure> measureList = measureRepository.findAll();
+    if (CollectionUtils.isNotEmpty(measureList)) {
+      setTempMeasures(measureList);
+      measureList.stream()
+          .forEach(
+              measure -> {
+                if (CollectionUtils.isNotEmpty(measure.getSupplementalData())) {
+                  StringBuffer sb = new StringBuffer();
+                  measure
+                      .getSupplementalData()
+                      .forEach(
+                          sde -> {
+                            sb.append(sde.getDefinition())
+                                .append("-")
+                                .append(sde.getDescription())
+                                .append(" | ");
+                          });
+                  if (measure.getMeasureMetaData() != null) {
+                    String sde =
+                        (measure.getMeasureMetaData().getSupplementalDataElements() != null
+                            ? measure.getMeasureMetaData().getSupplementalDataElements()
+                                + "; "
+                                + sb.toString()
+                            : sb.toString());
+                    measure.getMeasureMetaData().setSupplementalDataElements(sde);
+                  } else {
+                    MeasureMetaData metaData =
+                        MeasureMetaData.builder().supplementalDataElements(sb.toString()).build();
+                    measure.setMeasureMetaData(metaData);
+                  }
+
+                  measureRepository.save(measure);
+                }
+              });
+    }
+  }
+
+  @RollbackExecution
+  public void rollbackExecution(MeasureRepository measureRepository) throws Exception {
+    log.debug("Entering rollbackExecution()");
+    if (CollectionUtils.isNotEmpty(tempMeasures)) {
+      tempMeasures.forEach(
+          measure -> {
+            measureRepository.save(measure);
+          });
+    }
+  }
+}
