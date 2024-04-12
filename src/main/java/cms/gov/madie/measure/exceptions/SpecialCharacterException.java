d813c4e352355e68e252b0e1e67e7450fdaf6efc
CeciliaLiu8 <cecilialiu8@gmail.com>
2024-04-12 11:21:40 -0500

    MAT-6884 show special characters in message

--- src/main/java/cms/gov/madie/measure/exceptions/SpecialCharacterException.java
+++ src/main/java/cms/gov/madie/measure/exceptions/SpecialCharacterException.java
@@ -3,9 +3,10 @@ package cms.gov.madie.measure.exceptions;
 public class SpecialCharacterException extends RuntimeException {
   private static final long serialVersionUID = 4113761000508937686L;
 
-  private static final String MESSAGE = "Test Case %s can not contain special characters";
+  private static final String MESSAGE =
+      "Test Case %s can not contain special characters: (){}[]<>/|\"':;,.~`!@#$%^&*_+=\\";
 
   public SpecialCharacterException(String type) {
-    super(String.format(MESSAGE, type));
+    super(MESSAGE.replace("%s", type));
   }
 }
