18c6c1ebbdc6a8ece74000ba783300e1a2c9a855
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2024-07-30 10:29:05 -0700

    MAT-7475: remove unneeded feature flags

--- src/main/java/cms/gov/madie/measure/dto/MadieFeatureFlag.java
+++ src/main/java/cms/gov/madie/measure/dto/MadieFeatureFlag.java
@@ -2,7 +2,6 @@ package cms.gov.madie.measure.dto;
 
 /** Feature flags relevant to the measure-service */
 public enum MadieFeatureFlag {
-  QDM_EXPORT("qdmExport"),
   QDM_TEST_CASES("qdmTestCases"),
   IMPORT_TEST_CASES("importTestCases"),
   ENABLE_QDM_REPEAT_TRANSFER("enableQdmRepeatTransfer");
