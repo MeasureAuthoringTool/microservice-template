5adf8c6fae3400250a53c0eb4d35fb749ac7f636
Rohit Kandimalla <rohit.kandimalla@icf.com>
2024-10-14 11:08:15 -0400

    MAT-7602 Assigning Sequential Case Ids for all existing Test Cases

---
+++ src/test/java/cms/gov/madie/measure/config/AssignTestCaseSequentialIdTest.java
@@ -0,0 +1,94 @@
+package cms.gov.madie.measure.config;
+
+import cms.gov.madie.measure.repositories.MeasureRepository;
+import gov.cms.madie.models.common.ModelType;
+import gov.cms.madie.models.measure.Measure;
+import gov.cms.madie.models.measure.TestCase;
+import org.hl7.elm.r1.In;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AssignTestCaseSequentialIdTest {
+
+  @Mock MeasureRepository measureRepository;
+
+  @InjectMocks AssignTestCaseSequentialId assignTestCaseSequentialId;
+
+  Measure measure1;
+  TestCase tc1;
+  TestCase tc2;
+  TestCase tc3;
+  TestCase tc4;
+  TestCase tc5;
+  TestCase tc6;
+
+  @Captor private ArgumentCaptor<Measure> measureArgumentCaptor1;
+
+  @BeforeEach
+  public void setup() {
+    tc5 = TestCase.builder().id("TC5").name("TC%").createdAt(Instant.now()).build();
+    tc1 = TestCase.builder().id("TC1").name("TC1").createdAt(Instant.now()).build();
+    tc4 = TestCase.builder().id("TC4").name("TC4").createdAt(Instant.now()).build();
+    tc3 = TestCase.builder().id("TC3").name("TC3").createdAt(Instant.now()).build();
+    tc6 = TestCase.builder().id("TC6").name("TC6").createdAt(Instant.now()).build();
+    tc2 = TestCase.builder().id("TC2").name("TC2").createdAt(Instant.now()).build();
+
+    measure1 =
+        Measure.builder()
+            .id("Measure1")
+            .measureName("Measure1")
+            .model(ModelType.QI_CORE.getValue())
+            .testCases(List.of(tc1, tc2, tc3, tc4, tc5, tc6))
+            .build();
+  }
+
+  @Test
+  void testChangeUnitExecutionEmptyRepository() {
+    when(measureRepository.findAll()).thenReturn(List.of());
+    assignTestCaseSequentialId.updateTestCaseWithSequentialCaseNumber(measureRepository);
+    verifyNoMoreInteractions(measureRepository);
+  }
+
+  @Test
+  void assignTestCaseSequentialIdForAllMeasures() {
+    when(measureRepository.findAll()).thenReturn(List.of(measure1));
+
+    assignTestCaseSequentialId.updateTestCaseWithSequentialCaseNumber(measureRepository);
+    verify(measureRepository, times(1)).save(measureArgumentCaptor1.capture());
+    Measure measure = measureArgumentCaptor1.getValue();
+    assertThat(measure.getTestCases(), is(notNullValue()));
+    assertThat(measure.getTestCases().size(), is(equalTo(6)));
+    List<TestCase> sortedTestCases =
+        measure.getTestCases().stream()
+            .sorted(Comparator.comparing(TestCase::getCreatedAt))
+            .toList();
+    assertEquals("TC5", sortedTestCases.get(0).getId());
+    assertEquals(1, sortedTestCases.get(0).getCaseNumber());
+    assertEquals("TC1", sortedTestCases.get(1).getId());
+    assertEquals(2, sortedTestCases.get(1).getCaseNumber());
+    assertEquals("TC4", sortedTestCases.get(2).getId());
+    assertEquals(3, sortedTestCases.get(2).getCaseNumber());
+    assertEquals("TC3", sortedTestCases.get(3).getId());
+    assertEquals(4, sortedTestCases.get(3).getCaseNumber());
+    assertEquals("TC6", sortedTestCases.get(4).getId());
+    assertEquals(5, sortedTestCases.get(4).getCaseNumber());
+    assertEquals("TC2", sortedTestCases.get(5).getId());
+    assertEquals(6, sortedTestCases.get(5).getCaseNumber());
+  }
+}
