6c503f660c0742779ea734155f59da259bf59e2b
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2024-12-17 14:00:17 -0500

    MAT-7961: Trim the eCQM Abbr Title when used in an export filename.
    Signed-off-by: Joseph Kotanchik <joseph.kotanchik@semanticbits.com>

--- src/main/java/cms/gov/madie/measure/utils/ExportFileNamesUtil.java
+++ src/main/java/cms/gov/madie/measure/utils/ExportFileNamesUtil.java
@@ -6,12 +6,12 @@ public class ExportFileNamesUtil {
 
   public static String getExportFileName(Measure measure) {
     if (measure.getModel().startsWith("QI-Core")) {
-      return measure.getEcqmTitle() + "-v" + measure.getVersion() + "-FHIR";
+      return measure.getEcqmTitle().trim() + "-v" + measure.getVersion() + "-FHIR";
     }
-    return measure.getEcqmTitle() + "-v" + measure.getVersion() + "-" + measure.getModel();
+    return measure.getEcqmTitle().trim() + "-v" + measure.getVersion() + "-" + measure.getModel();
   }
 
   public static String getTestCaseExportZipName(Measure measure) {
-    return measure.getEcqmTitle() + "-v" + measure.getVersion().toString() + "-TestCases";
+    return measure.getEcqmTitle().trim() + "-v" + measure.getVersion().toString() + "-TestCases";
   }
 }
